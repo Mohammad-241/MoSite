fac00bc0fa1b20e5b3061c9e6cccc3462b0f31d2
Mohammad-241 <43042488+Mohammad-241@users.noreply.github.com>
2018-10-23 12:27:30 -0400

    added button codes

---
+++ Js/main.js
@@ -0,0 +1,40 @@
+function test1(){
+
+	var fName = prompt("Hey what's your name?");
+	var myColour = prompt("Pick a colour, " + fName);
+
+	if(myColour == "red"){
+
+		console.log("ha. this guy likes red");
+	} 
+	else if(myColour == "green"){
+		console.log("ah, green. He's alright.");
+	}
+	else if(myColour == "yellow"){
+		console.log("het him something yellow.");
+	}
+	else {
+		console.log("that's not a color");
+	}
+}
+
+function test2(){    
+	var age=prompt("how old are you?");
+	
+
+	if(age < 14){
+		alert("GET OUUUUTTT");
+
+	}
+	else if(age == 14){
+		alert("14 is a good age");
+
+	}
+	else if(age > 14){
+		alert("YOU ARE IIIINNNN");
+
+	}
+	else{
+		alert("Not a valid age");
+	}
+}
