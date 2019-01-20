d6c053c66ca94e981991a7ad8bf3249cef16feee
MohammadShahid241 <32366287+MohammadShahid241@users.noreply.github.com>
2019-01-20 18:36:38 -0500

    ooo

---
+++ java.js
@@ -0,0 +1,124 @@
+function test1(){
+
+	var fName = prompt("Hey what's your name?");
+	var myColour = prompt("Pick a colour, " + fName);
+
+	if(myColour == "red"){
+
+		console.log("ha. this guy likes red");
+	} 
+	else if(myColour == "green"){
+		console.log("ah, green. He's alright.");
+	}
+	else if(myColour == "yellow"){
+		console.log("het him something yellow.");
+	}
+	else {
+		console.log("that's not a color");
+	}
+}
+
+function test2(){    
+	var age=prompt("how old are you?");
+	
+
+	if(age < 14){
+		alert("GET OUUUUTTT");
+
+	}
+	else if(age == 14){
+		alert("14 is a good age");
+
+	}
+	else if(age > 14){
+		alert("YOU ARE IIIINNNN");
+
+	}
+	else{
+		alert("Not a valid age");
+	}
+}
+
+
+function FizzBuzz(){
+for ( var i = 1; i <= 100; i++ )
+{
+  if ( i%3 === 0 && i%5 === 0 )
+  {
+    console.log( i + " FizzBuzz" );
+  }
+  else if ( i%3 === 0 ) 
+  {
+    console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+  {
+    console.log(i+ " Buzz" );
+  }
+  else
+  {
+    console.log(i);
+  }
+  
+function test1(){
+
+	var fName = prompt("Hey what's your name?");
+	var myColour = prompt("Pick a colour, " + fName);
+
+	if(myColour == "red"){
+
+		console.log("ha. this guy likes red");
+	} 
+	else if(myColour == "green"){
+		console.log("ah, green. He's alright.");
+	}
+	else if(myColour == "yellow"){
+		console.log("het him something yellow.");
+	}
+	else {
+		console.log("that's not a color");
+	}
+}
+
+function test2(){    
+	var age=prompt("how old are you?");
+	
+
+	if(age < 14){
+		alert("GET OUUUUTTT");
+
+	}
+	else if(age == 14){
+		alert("14 is a good age");
+
+	}
+	else if(age > 14){
+		alert("YOU ARE IIIINNNN");
+
+	}
+	else{
+		alert("Not a valid age");
+	}
+}
+
+
+function Fizzbuzz(){
+for ( var i = 1; i <= 100; i++ )
+{
+  if ( i%3 === 0 && i%5 === 0 )
+  {
+    console.log( i + " FizzBuzz" );
+  }
+  else if ( i%3 === 0 ) 
+  {
+    console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+  {
+    console.log(i+ " Buzz" );
+  }
+  else
+  {
+    console.log(i);
+  }
+}
